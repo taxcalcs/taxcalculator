3493a9a282717cd5300172529cf1301c39527b9e
Ariel Kuechler <github.ariel@kuechler.info>
2016-01-20 09:26:35 +0100

    #8 add TaxCalculatorFactory.getYearKey() + Test

--- src/test/java/info/kuechler/bmf/taxcalculator/rw/FactoryTest.java
+++ src/test/java/info/kuechler/bmf/taxcalculator/rw/FactoryTest.java
@@ -12,6 +12,10 @@ import org.slf4j.LoggerFactory;
 public class FactoryTest {
     private final static Logger LOG = LoggerFactory.getLogger(FactoryTest.class);
 
+    /**
+     * Simple Test for general features.
+     * @throws ReadWriteException
+     */
     @Test
     public final void test() throws ReadWriteException {
         final TaxCalculatorFactory factory = new TaxCalculatorFactory();
@@ -39,5 +43,55 @@ public class FactoryTest {
         for (final String in : factory.getOutputs("2015Dezember")) {
             LOG.debug(in);
         }
+    }
+    
+    /**
+     * Test {@link TaxCalculatorFactory#getYearKey(int, int)}.
+     */
+    @Test
+    public final void testGetYearKey() {
+        final TaxCalculatorFactory factory = new TaxCalculatorFactory();
+        Assert.assertEquals("2006", factory.getYearKey(0, 2006));
+        Assert.assertEquals("2006", factory.getYearKey(5, 2006));
+        Assert.assertEquals("2006", factory.getYearKey(12, 2006));
+        
+        Assert.assertEquals("2007", factory.getYearKey(0, 2007));
+        Assert.assertEquals("2007", factory.getYearKey(5, 2007));
+        Assert.assertEquals("2007", factory.getYearKey(12, 2007));
+        
+        Assert.assertEquals("2008", factory.getYearKey(0, 2008));
+        Assert.assertEquals("2008", factory.getYearKey(5, 2008));
+        Assert.assertEquals("2008", factory.getYearKey(12, 2008));
+        
+        Assert.assertEquals("2009", factory.getYearKey(0, 2009));
+        Assert.assertEquals("2009", factory.getYearKey(5, 2009));
+        Assert.assertEquals("2009", factory.getYearKey(12, 2009));
+        
+        Assert.assertEquals("2010", factory.getYearKey(0, 2010));
+        Assert.assertEquals("2010", factory.getYearKey(5, 2010));
+        Assert.assertEquals("2010", factory.getYearKey(12, 2010));
+        
+        Assert.assertEquals("2011December", factory.getYearKey(0, 2011));
+        Assert.assertEquals("2011November", factory.getYearKey(5, 2011));
+        Assert.assertEquals("2011December", factory.getYearKey(12, 2011));
+        
+        Assert.assertEquals("2012", factory.getYearKey(0, 2012));
+        Assert.assertEquals("2012", factory.getYearKey(5, 2012));
+        Assert.assertEquals("2012", factory.getYearKey(12, 2012));
+        
+        Assert.assertEquals("2013", factory.getYearKey(0, 2013));
+        Assert.assertEquals("2013", factory.getYearKey(5, 2013));
+        Assert.assertEquals("2013", factory.getYearKey(12, 2013));
+        
+        Assert.assertEquals("2014", factory.getYearKey(0, 2014));
+        Assert.assertEquals("2014", factory.getYearKey(5, 2014));
+        Assert.assertEquals("2014", factory.getYearKey(12, 2014));
+        
+        Assert.assertEquals("2015Dezember", factory.getYearKey(0, 2015));
+        Assert.assertEquals("2015", factory.getYearKey(5, 2015));
+        Assert.assertEquals("2015Dezember", factory.getYearKey(12, 2015));
+        
+        Assert.assertEquals("2016", factory.getYearKey(0, 2016));
+        Assert.assertEquals("2016", factory.getYearKey(12, 2016));
     }
 }
