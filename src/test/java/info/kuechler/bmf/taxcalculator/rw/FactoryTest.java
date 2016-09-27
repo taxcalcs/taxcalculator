4f5b75479f6bf839b461d8202594c333c0188a23
Ariel Kuechler <github.ariel@kuechler.info>
2016-09-27 23:53:09 +0200

    #16 add input/output methods with type

--- src/test/java/info/kuechler/bmf/taxcalculator/rw/FactoryTest.java
+++ src/test/java/info/kuechler/bmf/taxcalculator/rw/FactoryTest.java
@@ -14,6 +14,7 @@ public class FactoryTest {
 
     /**
      * Simple Test for general features.
+     * 
      * @throws ReadWriteException
      */
     @Test
@@ -45,7 +46,7 @@ public class FactoryTest {
             Assert.assertNotEquals("'CLASS' getter found.", "CLASS", in);
         }
     }
-    
+
     /**
      * Test {@link TaxCalculatorFactory#getYearKey(int, int)}.
      */
@@ -55,44 +56,76 @@ public class FactoryTest {
         Assert.assertEquals("2006", factory.getYearKey(0, 2006));
         Assert.assertEquals("2006", factory.getYearKey(5, 2006));
         Assert.assertEquals("2006", factory.getYearKey(12, 2006));
-        
+
         Assert.assertEquals("2007", factory.getYearKey(0, 2007));
         Assert.assertEquals("2007", factory.getYearKey(5, 2007));
         Assert.assertEquals("2007", factory.getYearKey(12, 2007));
-        
+
         Assert.assertEquals("2008", factory.getYearKey(0, 2008));
         Assert.assertEquals("2008", factory.getYearKey(5, 2008));
         Assert.assertEquals("2008", factory.getYearKey(12, 2008));
-        
+
         Assert.assertEquals("2009", factory.getYearKey(0, 2009));
         Assert.assertEquals("2009", factory.getYearKey(5, 2009));
         Assert.assertEquals("2009", factory.getYearKey(12, 2009));
-        
+
         Assert.assertEquals("2010", factory.getYearKey(0, 2010));
         Assert.assertEquals("2010", factory.getYearKey(5, 2010));
         Assert.assertEquals("2010", factory.getYearKey(12, 2010));
-        
+
         Assert.assertEquals("2011December", factory.getYearKey(0, 2011));
         Assert.assertEquals("2011November", factory.getYearKey(5, 2011));
         Assert.assertEquals("2011December", factory.getYearKey(12, 2011));
-        
+
         Assert.assertEquals("2012", factory.getYearKey(0, 2012));
         Assert.assertEquals("2012", factory.getYearKey(5, 2012));
         Assert.assertEquals("2012", factory.getYearKey(12, 2012));
-        
+
         Assert.assertEquals("2013", factory.getYearKey(0, 2013));
         Assert.assertEquals("2013", factory.getYearKey(5, 2013));
         Assert.assertEquals("2013", factory.getYearKey(12, 2013));
-        
+
         Assert.assertEquals("2014", factory.getYearKey(0, 2014));
         Assert.assertEquals("2014", factory.getYearKey(5, 2014));
         Assert.assertEquals("2014", factory.getYearKey(12, 2014));
-        
+
         Assert.assertEquals("2015Dezember", factory.getYearKey(0, 2015));
         Assert.assertEquals("2015", factory.getYearKey(5, 2015));
         Assert.assertEquals("2015Dezember", factory.getYearKey(12, 2015));
-        
+
         Assert.assertEquals("2016", factory.getYearKey(0, 2016));
         Assert.assertEquals("2016", factory.getYearKey(12, 2016));
+    }
+
+    /**
+     * Test {@link AbstractReadWriteFactory#getInputsWithType(String)}.
+     * 
+     * @throws ReadWriteException
+     */
+    @Test
+    public final void testGetInputsWithType() throws ReadWriteException {
+        final TaxCalculatorFactory factory = new TaxCalculatorFactory();
+        final Map<String, Class<?>> inputs = factory.getInputsWithType(factory.getYearKey(1, 2016));
+        for (final Map.Entry<String, Class<?>> entry : inputs.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().getName());
+            Assert.assertTrue(entry.getValue() == BigDecimal.class || entry.getValue() == int.class
+                    || entry.getValue() == double.class);
+        }
+    }
+
+    /**
+     * Test {@link AbstractReadWriteFactory#getOutputsWithType(String)}.
+     * 
+     * @throws ReadWriteException
+     */
+    @Test
+    public final void testGetOutputsWithType() throws ReadWriteException {
+        final TaxCalculatorFactory factory = new TaxCalculatorFactory();
+        final Map<String, Class<?>> outputs = factory.getOutputsWithType(factory.getYearKey(1, 2016));
+        for (final Map.Entry<String, Class<?>> entry : outputs.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().getName());
+            Assert.assertTrue(entry.getValue() == BigDecimal.class || entry.getValue() == int.class
+                    || entry.getValue() == double.class);
+        }
     }
 }
